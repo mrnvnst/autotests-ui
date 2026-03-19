94bd9578514c02c81b708dc57d6702165b403c0e
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-19 16:53:51 +0300

    add pytest.mark.usefuxtures example

---
+++ tests/test_usefixtures.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+# Фикстура для очистки данных из базы данных
+@pytest.fixture
+def clear_books_database():
+    print("[FIXTURE] Удаляем все данные из базы данных")
+
+
+# Фикстура для заполнения данных в базу данных
+@pytest.fixture
+def fill_books_database():
+    print("[FIXTURE] Создаем новые данные в базе данных")
+
+
+@pytest.mark.userfixtures('fill_books_database')
+# [FIXTURE] Создаем новые данные в базе данных
+def test_read_all_books_in_library():
+    ...
+
+
+@pytest.mark.userfixtures(
+    'clear_books_database',
+    'fill_books_database'
+)
+class TestLibrary:
+    # [FIXTURE] Удаляем все данные из базы данных
+    # [FIXTURE] Создаем новые данные в базе данных
+    def test_read_book_from_library(self):
+        ...
+
+    # [FIXTURE] Удаляем все данные из базы данных
+    # [FIXTURE] Создаем новые данные в базе данных
+    def test_delete_book_from_library(self):
+        ...
+
