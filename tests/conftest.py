69cae6e33baf58f498c664dff71dcc8d9d455637
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-19 17:27:29 +0300

    use pytest-playwright for browser control

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,10 @@
 import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, Page  # Имопртируем класс страницы, будем использовать его для аннотации типов
+from playwright.sync_api import sync_playwright, Page, Playwright
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page() -> Page:  # Аннотируем возвращаемое фикстурой значение
-    # Ниже идет инициализация и открытие новой страницы
-    with sync_playwright() as playwright:
-        # Запускаем браузер
-        browser = playwright.chromium.launch(headless=False)
-
-        # Передаем страницу для использования в тесте
-        yield browser.new_page()
-
-        # Закрываем браузер после выполнения тестов
-        browser.close()
-
+@pytest.fixture
+# Используем встроенную фикстуру playwright (установлена с плагином pytest-playwright)
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
