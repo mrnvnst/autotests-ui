12021184b119b20c7256cae76fe200a70f9f301d
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-19 14:27:58 +0300

    add test for checking courses list

---
+++ tests/test_courses.py
@@ -0,0 +1,52 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_empty_courses_list():
+    with sync_playwright() as playwright:
+
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        # Добавила проверку на видимость заголовка Dashboard, т.к. токен не успевал записываться в .json
+        dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+        expect(dashboard_title).to_be_visible()
+
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+        course_title = page.get_by_test_id('courses-list-toolbar-title-text')
+        expect(course_title).to_be_visible()
+        expect(course_title).to_have_text('Courses')
+
+        empty_list_block = page.get_by_test_id('courses-list-empty-view-title-text')
+        expect(empty_list_block).to_be_visible()
+        expect(empty_list_block).to_have_text('There is no results')
+
+        empty_list_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        expect(empty_list_icon).to_be_visible()
+
+        empty_list_description = page.get_by_test_id('courses-list-empty-view-description-text')
+        expect(empty_list_description).to_be_visible()
+        expect(empty_list_description).to_have_text('Results from the load test pipeline will be displayed here')
+
