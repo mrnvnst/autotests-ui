a6c80d9405f32f5c2be00f5412951275bed01bc0
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-22 22:06:25 +0300

    add fixtures: initialize browser and opening page with saved state

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, Page, Playwright
+from playwright.sync_api import Page, Playwright, expect
 
 
 @pytest.fixture
@@ -8,3 +8,39 @@ def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
+
+
+# Регистрация нового пользователя через страницу регистрации, после регистрации сохраняем состояние браузера в файл
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(dashboard_title).to_be_visible()
+
+    context.storage_state(path='browser-state.json')
+    browser.close()
+
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    yield context.new_page()
+    browser.close()
+
