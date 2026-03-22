e924caffdc81fc352857c0102a05ad1d1d68208c
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-23 01:11:13 +0300

    add parametrization for invalid email - password test

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,19 +2,27 @@ import pytest
 from playwright.sync_api import expect, Page
 
 
+login_data = {
+    ("user.name@gmail.com", "password"): "Invalid email and password",
+    ("user.name@gmail.com", "  "): "Invalid email and empty password",
+    ("  ", "password"): "Empty email and invalid password"
+}
+
+
 @pytest.mark.regression
 @pytest.mark.authorization
+@pytest.mark.parametrize("email, password", login_data.keys(), ids=login_data.values())
 # Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
 
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+    email_input.fill(email)
 
     password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill('password')
+    password_input.fill(password)
 
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
