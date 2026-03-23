08a6de89abb0e370ea6cdc44664956fa53d512bb
Anastasiia Mironova <ananmironova97@gmail.com>
2026-03-23 17:51:44 +0300

    create fixtures for pages initializing

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,11 +1,23 @@
 import pytest
 
 from playwright.sync_api import Page
+from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
 
 
 # Инициализация страницы через фикстуру
 @pytest.fixture
-def login_page(chromium_page: Page) -> Page:
+def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture(scope='function')
+def registration_page(chromium_page: Page) -> RegistrationPage:
+    return RegistrationPage(page=chromium_page)
+
+
+@pytest.fixture(scope='function')
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
 
